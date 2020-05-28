041d38e9e49164a46f90fce527a68c4c2ee60964
yihansix6 <1833423314@qq.com>
2020-05-28 20:22:47 +0800

    ‘项目初始化

---
+++ Microblog/Controllers/ChengController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Microblog.Controllers
+{
+    public class ChengController : Controller
+    {
+        // GET: Cheng
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns>视图</returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Microblog/Controllers/TangController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Microblog.Controllers
+{
+    public class TangController : Controller
+    {
+        // GET: Tang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
